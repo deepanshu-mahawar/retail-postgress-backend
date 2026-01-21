5edce76ef61c3a954ae9ebe5642146606314a7f1
abhishek-vermacodes <abhishek.verma@kriotek.in>
2026-01-21 10:12:32 +0530

    add abhishek changes

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -7,11 +7,11 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST || "localhost",
-  port: Number(process.env.DB_PORT || "5432"),
-  username: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "911918",
-  database: process.env.DB_NAME || "retail-pro",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: "123456",
+  database: process.env.DB_NAME,
   synchronize: true,
   logging: false,
   entities: [User],
